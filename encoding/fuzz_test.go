47c0b8aace1476621c8d10ddf6da4908bba5814f
Julien Fabre <ju.pryz@gmail.com>
2022-01-24 11:57:15 -0700

    parquet -> parquet-go

--- encoding/fuzz_test.go
+++ encoding/fuzz_test.go
@@ -9,12 +9,12 @@ import (
 	"io"
 	"testing"
 
-	"github.com/segmentio/parquet/encoding"
-	"github.com/segmentio/parquet/encoding/bytestreamsplit"
-	"github.com/segmentio/parquet/encoding/delta"
-	"github.com/segmentio/parquet/encoding/plain"
-	"github.com/segmentio/parquet/encoding/rle"
-	"github.com/segmentio/parquet/format"
+	"github.com/segmentio/parquet-go/encoding"
+	"github.com/segmentio/parquet-go/encoding/bytestreamsplit"
+	"github.com/segmentio/parquet-go/encoding/delta"
+	"github.com/segmentio/parquet-go/encoding/plain"
+	"github.com/segmentio/parquet-go/encoding/rle"
+	"github.com/segmentio/parquet-go/format"
 )
 
 func fuzzEncoding(fuzz func(e encoding.Encoding)) {
